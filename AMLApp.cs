25b67389bcdd2de40e6bb90f55631429126b4181
Josef <josef.prinz@inpro.de>
2023-02-06 08:59:13 +0100

    code cleaning

--- AMLApp.cs
+++ AMLApp.cs
@@ -1,65 +1,61 @@
-﻿
-using ControlzEx.Theming;
+﻿#nullable disable
 using System;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
+using ControlzEx.Theming;
 
-#nullable disable
-namespace Aml.Skins
+namespace Aml.Skins;
+
+public static class AMLApp
 {
-    public static class AMLApp
+    public static Action<ResourceDictionary> ApplyColors { get; set; }
+    public static ResourceDictionary ThemeColors { get; set; }
+
+
+    public static bool IsDarkMode { get; private set; }
+
+    /// <summary>
+    ///     Applies the skin to the application
+    /// </summary>
+    /// <param name="app">The application.</param>
+    /// <param name="skinDictionaryUri">The skin dictionary URI.</param>
+    public static void ApplySkin(Application app, Uri skinDictionaryUri)
     {
-        public static Action<ResourceDictionary> ApplyColors { get; set; }
-        public static ResourceDictionary ThemeColors { get; set; }
+        // ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.
 
+        // Load the ResourceDictionary into memory.
+        var skinDict = Application.LoadComponent(skinDictionaryUri) as ResourceDictionary;
 
-        public static bool IsDarkMode {get; private set;}
+        var mergedDicts = app.Resources.MergedDictionaries;
 
-          /// <summary>
-        /// Applies the skin to the application
-        /// </summary>
-        /// <param name="app">The application.</param>
-        /// <param name="skinDictionaryUri">The skin dictionary URI.</param>
-        public static void ApplySkin(Application app, Uri skinDictionaryUri)
+        var skinSource = @"/Aml.Skins;component/Themes";
+
+        // Remove the existing skin dictionary, if one exists.
+        foreach (var dict in mergedDicts.ToList())
         {
-            // ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.
-
-            // Load the ResourceDictionary into memory.
-            var skinDict = Application.LoadComponent(skinDictionaryUri) as ResourceDictionary;
-
-            var mergedDicts = app.Resources.MergedDictionaries;
-
-            var skinSource = @"/Aml.Skins;component/Themes";
-
-            // Remove the existing skin dictionary, if one exists.
-            foreach (var dict in mergedDicts.ToList())
+            if (dict.Source != null && dict.Source.OriginalString.Contains(skinSource))
             {
-                if (dict.Source != null && dict.Source.OriginalString.Contains(skinSource))
-                {
-                    mergedDicts.Remove(dict);
-                }
+                mergedDicts.Remove(dict);
             }
-
-            // Apply the selected skin so that all elements in the
-            // application will honor the new look and feel.
-            mergedDicts.Add(skinDict);
-
-            var theme = skinDictionaryUri.OriginalString.Contains ("Dark.Styles") ? "Dark" : "Light";
-            ThemeManager.Current.ChangeThemeBaseColor(Application.Current, theme);
-
-            IsDarkMode = theme == "Dark";
-            SetColors (theme == "Dark");
         }
 
-        public static void SetColors (bool isDarkMode)
-        {
-            ThemeColors = (isDarkMode)
-                ? new ResourceDictionary {Source = SkinSource.DarkSkinColors }
-                : new ResourceDictionary {Source =SkinSource.LightSkinColors };
-            
-            ApplyColors?.Invoke (ThemeColors);
-        }
+        // Apply the selected skin so that all elements in the
+        // application will honor the new look and feel.
+        mergedDicts.Add(skinDict);
 
+        var theme = skinDictionaryUri.OriginalString.Contains("Dark.Styles") ? "Dark" : "Light";
+        ThemeManager.Current.ChangeThemeBaseColor(Application.Current, theme);
+
+        IsDarkMode = theme == "Dark";
+        SetColors(theme == "Dark");
+    }
+
+    public static void SetColors(bool isDarkMode)
+    {
+        ThemeColors = isDarkMode
+            ? new ResourceDictionary { Source = SkinSource.DarkSkinColors }
+            : new ResourceDictionary { Source = SkinSource.LightSkinColors };
+
+        ApplyColors?.Invoke(ThemeColors);
     }
 }
--- SkinSource.cs
+++ SkinSource.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 
-namespace Aml.Skins
+namespace Aml.Skins;
+
+/// <summary>
+///     The skin source.
+/// </summary>
+public class SkinSource
 {
     /// <summary>
-    /// The skin source.
+    ///     Prevents a default instance of the <see cref="SkinSource" /> class from being created.
     /// </summary>
-    public class SkinSource
+    private SkinSource()
     {
-        public static SkinSource Instance { get; } = new SkinSource();
-        public static Uri DarkSkin { get; } = new Uri("/Aml.Skins;component/Themes/Dark.Styles.xaml", UriKind.Relative);
-        public static Uri LightSkin { get; } = new Uri("/Aml.Skins;component/Themes/Light.Styles.xaml", UriKind.Relative);
-        public static Uri DarkSkinColors  { get; } = new Uri("/Aml.Skins;component/Themes/Dark.Theme.Colors.xaml", UriKind.Relative);
-        public static Uri LightSkinColors { get; } = new Uri("/Aml.Skins;component/Themes/Light.Theme.Colors.xaml", UriKind.Relative);
-
-
-        /// <summary>
-        /// Prevents a default instance of the <see cref="SkinSource"/> class from being created.
-        /// </summary>
-        private SkinSource()
-        { }
     }
+
+    public static SkinSource Instance { get; } = new();
+    public static Uri DarkSkin { get; } = new("/Aml.Skins;component/Themes/Dark.Styles.xaml", UriKind.Relative);
+    public static Uri LightSkin { get; } = new("/Aml.Skins;component/Themes/Light.Styles.xaml", UriKind.Relative);
+
+    public static Uri DarkSkinColors { get; } =
+        new("/Aml.Skins;component/Themes/Dark.Theme.Colors.xaml", UriKind.Relative);
+
+    public static Uri LightSkinColors { get; } =
+        new("/Aml.Skins;component/Themes/Light.Theme.Colors.xaml", UriKind.Relative);
 }
