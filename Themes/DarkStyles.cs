d32823f81ecad122d757654e5d435f58b229f341
JosefPrinz <home@berlin-prinz.de>
2024-07-20 22:06:58 +0200

    bug fix in 6.3.0

---
+++ Themes/DarkStyles.cs
@@ -0,0 +1,22 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Aml.Skins.Themes
+{
+    public partial class DarkStyles
+    {
+        void CheckBoxChecked (object sender, EventArgs e)
+        {
+            var rb = CheckBoxHelper.GetCheckGlyphForegroundChecked(sender as DependencyObject);
+            CheckBoxHelper.SetCheckGlyphForegroundChecked (sender as DependencyObject, Brushes.White);
+            var h = CheckBoxHelper.CheckGlyphForegroundCheckedProperty;
+        }
+    }
+}
